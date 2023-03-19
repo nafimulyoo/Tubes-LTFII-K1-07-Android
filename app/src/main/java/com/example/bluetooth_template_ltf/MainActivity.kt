2ffbb2a2a6ef950dbac4e95193654b21317a862f
Muhammad Sulthan Mazaya <msulthanmazaya@gmail.com>
2023-03-19 09:52:14 +0700

    add joystick for tubes

--- app/src/main/java/com/example/bluetooth_template_ltf/MainActivity.kt
+++ app/src/main/java/com/example/bluetooth_template_ltf/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.bluetooth_template_ltf
 
-import android.Manifest
-import android.bluetooth.BluetoothDevice
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.widget.Button
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.bluetooth_template_ltf.databinding.ActivityMainBinding
 import com.example.bluetooth_template_ltf.helperBT.BTActivityWrapper
-import com.harrysoft.androidbluetoothserial.BluetoothManager
-import com.harrysoft.androidbluetoothserial.BluetoothSerialDevice
-import com.harrysoft.androidbluetoothserial.SimpleBluetoothDeviceInterface
-import com.jakewharton.rxbinding3.view.drags
-import com.jakewharton.rxbinding3.view.layoutChanges
-import com.jakewharton.rxbinding3.view.systemUiVisibilityChanges
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.*
+import io.reactivex.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
-import kotlin.properties.Delegates
-import com.jakewharton.rxbinding3.widget.changes
 
 
 class MainActivity : BTActivityWrapper() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private var lastMessageSentTime: Long = 0
+    private val joystickMoveSubject = PublishSubject.create<Pair<Int, Int>>()
+    private var prevAngle: Int = 0
+    private var prevStrength: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         initBluetooth()
 
@@ -49,19 +29,22 @@ class MainActivity : BTActivityWrapper() {
                 connectBluetooth(binding.inputBluetooth.text.toString())
         }
 
-        binding.slider.changes()
+        binding.joystickView.setOnMoveListener { angle, strength ->
+            if(angle != prevAngle || strength != prevStrength) {
+                joystickMoveSubject.onNext(angle to strength)
+                prevAngle = angle;
+                prevStrength = strength;
+            }
+        }
+
+        joystickMoveSubject
             .debounce(100, TimeUnit.MILLISECONDS)
-            .distinctUntilChanged()
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { value ->
-                var angleValue = (value.toDouble()/100)*180
-                var angleString = String.format("%.1f", angleValue)
-                if (connected) {
-                    sendBluetoothMessage(angleString)
-                    binding.angleText.text = "Angle: $angleString"
-                } else {
-                    alert("Tidak terkoneksi dengan bluetooth device")
-                }
+            .subscribe { (angle, strength) ->
+                binding.angleText.text = "Angle: $angle"
+                binding.magnitudeText.text = "Magnitude: $strength"
+                Log.d("TEST", "angle $angle strength $strength")
+//                sendBluetoothMessage("$angle;$strength")
             }
 
         // Ini buat check bluetooth connection,
@@ -70,10 +53,10 @@ class MainActivity : BTActivityWrapper() {
         val mainHandler = Handler(Looper.getMainLooper())
         mainHandler.post(object : Runnable {
             override fun run() {
-                if(connected && !binding.slider.isEnabled) {
-                    binding.slider.isEnabled = true
+                if(connected && !binding.joystickView.isEnabled) {
+                    binding.joystickView.isEnabled = true
                 } else if (!connected) {
-                    binding.slider.isEnabled = false
+                    binding.joystickView.isEnabled = false
                 }
                 mainHandler.postDelayed(this, 200)
             }
